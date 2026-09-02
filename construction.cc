e9dfa7f4a5139f093397b92759b6db9892a7208b
George <122010570+georgepata98@users.noreply.github.com>
2026-09-02 14:57:13 +0300

    Update construction.cc

--- construction.cc
+++ construction.cc
@@ -11,15 +11,14 @@ G4VPhysicalVolume* MyDetectorConstruction::Construct()
         G4NistManager *nist = G4NistManager::Instance();
         
         G4Material *air = nist->FindOrBuildMaterial("G4_AIR");
-        G4Material *water = nist->FindOrBuildMaterial("G4_WATER");
         
         G4Box *solidWorld = new G4Box("solidWorld", 25*cm, 25*cm, 25*cm);
         G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, air, "logicWorld");
         G4VPhysicalVolume *physWorld = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicWorld, "physWorld", 0, false, 0);
         
-        G4Box *sTarget = new G4Box("sTarget", 10*cm, 10*cm, 5*cm);  // placa de 20x20x10 cm
-        G4LogicalVolume *lTarget = new G4LogicalVolume(sTarget, water, "lTarget");
-        G4VPhysicalVolume *pTarget = new G4PVPlacement(0, G4ThreeVector(0, 0, 10*cm), lTarget, "pTarget", logicWorld, false, 0);
+        G4Box *sTarget = new G4Box("sTarget", 10*cm, 10*cm, 1*mm);  // tinta subtire pentru fluenta
+        G4LogicalVolume *lTarget = new G4LogicalVolume(sTarget, air, "lTarget");
+        G4VPhysicalVolume *pTarget = new G4PVPlacement(0, G4ThreeVector(0, 0, 5*cm), lTarget, "pTarget", logicWorld, false, 0);
         
         return physWorld;
 }
